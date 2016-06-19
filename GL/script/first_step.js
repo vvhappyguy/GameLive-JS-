83c7225f6f6aab130b932fe68ca863a5317e5258
Iowan <dyakin_ip@outlook.com>
2016-06-19 19:31:45 +0300

    Merge branch 'Memory' (reverted from commit 32f9f67a3ba3690cda258fdb24079f7cdbc8b8b7)

--- GL/script/first_step.js
+++ GL/script/first_step.js
@@ -1,22 +1,5 @@
 var po = 0; //memory for different points
 
-//starting filling array 20x20
-function matrixArray(rows,columns){
-  var mem = [];
-  for(var i=1; i<columns; i++){
-    mem[i] = new Array();
-    for(var j=1; j<rows; j++){
-      mem[i][j] = false;
-    }
-  }
-  console.log(mem);
-  console.log(typeof(mem));
-  console.log(typeof(mem[1][1]));
-  return mem;
-}
-var mem = matrixArray(21,21);
-
-
 function let(n){
 	changecolor(n);
 	memory(n);
@@ -25,18 +8,7 @@ function let(n){
 //function for remember points
 //with double array
 function memory(n,mem){
-	var	i = parseInt(n / 1000);
-	var	j = n % 100;
-    console.log(i);
-    console.log(j);	
-    console.log(mem[i][j]);
-    console.log(typeof(mem[i][j]));
-	if (mem[i][j]==0){
-		mem[i][j]=1;
-	}	else {
-		mem[i][j]=0;
-	}
-	 console.log(typeof(mem[i][j]));	
+	
 }	
 
 //function for taking points for next steps
