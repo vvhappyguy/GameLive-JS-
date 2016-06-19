b9d679b75c6c6c408e26c74f6f6b3d0c07fcfb1c
Iowan <dyakin_ip@outlook.com>
2016-06-19 19:47:24 +0300

    Memory for points is ready!

--- GL/script/first_step.js
+++ GL/script/first_step.js
@@ -1,9 +1,29 @@
 var po = 0; //memory for different points
 
+//starting filling array 20x20
+function matrixArray(rows,columns){
+  var mem = [];
+  for(var i=1; i<columns; i++){
+    mem[i] = new Array();
+    for(var j=1; j<rows; j++){
+      mem[i][j] = false;
+    }
+  }
+  return mem;
+}
+var mem = matrixArray(21,21);
+
 function let(n){
 	changecolor(n);
+	var	i = parseInt(n / 1000);
+	var	j = n % 100;
+	if(mem[i][j]==false){
+		mem[i][j]=true
+	}	else {
+		mem[i][j]=false
+	}
+	console.log(mem[i][j]);
 }
-
 
 //function for taking points for next steps
 function changecolor(n){
