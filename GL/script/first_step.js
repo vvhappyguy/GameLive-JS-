d306fb3b346fb42e67569e87727a29c8d334845c
Iowan <dyakin_ip@outlook.com>
2016-06-19 21:32:31 +0300

    Beta-version Only One Step

--- GL/script/first_step.js
+++ GL/script/first_step.js
@@ -1,64 +1,88 @@
 var po = 0; //memory for different points
+var st = 0;	//
+var stop = 0;
 
-var st=0;
 //starting filling array 20x20
-function matrixArray(rows,columns){
+function matrixArray(rows, columns) {
   var mem = [];
-  for(var i=1; i<columns; i++){
+  for (var i = 0; i < columns; i++) {
     mem[i] = new Array();
-    for(var j=1; j<rows; j++){
-      mem[i][j] = false;
+    for (var j = 0; j < rows; j++) {
+      mem[i][j] = 0;
     }
   }
   return mem;
 }
-var mem = matrixArray(21,21);
+var mem = matrixArray(22, 22);
 
-function let(n){
-	if (st == 0){
+function let(n) {
+	if (st == 0) {
 		changecolor(n);
 		var i = parseInt(n / 1000);
 		var j = n % 100;
-		if (mem[i][j] == false) {
-			mem[i][j] = true
+		if (mem[i][j] == 0) {
+			mem[i][j] = 1
 		} else {
-			mem[i][j] = false
+			mem[i][j] = 0
 		}
 		console.log(mem[i][j]);
 	}
 	console.log(st);
 }
 
-function readyBtn(n){
+function readyBtn(n) {
 	st = n;
 	console.log(st);
 }
 
-function newBtn(n){
+function newBtn(n) {
 	location.reload();
 }
 
-function game() {
-	while (true) {
-		for (var i = 1; i < 21; i++) {
-			for (var j = 1; j < 21; j++) {
-				
+function game(n) {
+	if (st == 1) {
+		if (n == 1) {
+			for (var i = 1; i < 21; i++) {
+				for (var j = 1; j < 21; j++) {
+					console.log(mem[i][j]);
+					var sum = mem[i - 1][j - 1] + mem[i - 1][j] + mem[i - 1][j + 1] + mem[i][j - 1] + mem[i][j + 1] + mem[i + 1][j - 1] + mem[i + 1][j] + mem[i + 1][j + 1];
+					var numP = 1000 * i + j;
+					console.log(numP);
+					console.log(sum);
+					po = $("#point" + numP).css("background-color");
+					if (po == "rgb(255, 255, 255)") {
+						if (sum == 3) {
+							mem[i][j] = 1;
+							$("#point" + numP).css("background", "rgb(0,0,0)");
+						}
+					} else if(po == "rgb(0, 0, 0)"){
+							if ( sum>1 && sum<4){
+								mem[i][j] = 1;
+								$("#point" + numP).css("background", "rgb(0,0,0)");
+							} else {
+								mem[i][j] = 0;
+								$("#point" + numP).css("background", "rgb(255,255,255)");
+							}
+					}
+				}
 			}
+		} else if (st == 2) {
+
 		}
 	}
 }
 
 //function for taking points for next steps
-function changecolor(n){
+function changecolor(n) {
 
-if (po != n){
-	po = $("#point"+n).css("background-color");
-	console.log(po);
-	if (po == "rgb(255, 255, 255)"){
-		$("#point"+n).css("background-color", "rgb(0, 0, 0)");
+	if (po != n) {
+		po = $("#point" + n).css("background-color");
+		console.log(po);
+		if (po == "rgb(255, 255, 255)") {
+			$("#point" + n).css("background-color", "rgb(0, 0, 0)");
+		}
+		else {
+			$("#point" + n).css("background", "rgb(255,255,255)");
+		}
 	}
-	else {
-		$("#point"+n).css("background", "rgb(255,255,255)");
-	}
-}
 }
